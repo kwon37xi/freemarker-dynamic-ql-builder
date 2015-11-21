eb847b4319222c172af40c10a5278083623104be
KwonNam Son <kwon37xi@gmail.com>
2015-11-21 16:40:41 +0900

    ParamMethod - change javadoc

--- src/main/java/kr/pe/kwonnam/fdqlbuilder/methods/ParamMethod.java
+++ src/main/java/kr/pe/kwonnam/fdqlbuilder/methods/ParamMethod.java
@@ -23,7 +23,7 @@ import static org.slf4j.LoggerFactory.getLogger;
  * <li>{@code converterName} is used to find {@link ParameterConverter} and convvert the parameter to another object.</li>
  * </ul>
  * <p/>
- * This is NOT thread safe. Each template processing request must create new {@link ParamMethod} object.
+ * This is NOT thread safe. Each template processing request must create new instance.
  *
  * @see ParameterConverter
  */
