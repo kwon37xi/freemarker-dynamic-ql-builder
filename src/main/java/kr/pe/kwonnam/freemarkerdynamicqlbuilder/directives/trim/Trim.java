1d448ec6e7d6963b16aa90ba5434131d058ffbc6
KwonNam Son <kwon37xi@gmail.com>
2015-11-22 21:24:08 +0900

    Trim completed

---
+++ src/main/java/kr/pe/kwonnam/freemarkerdynamicqlbuilder/directives/trim/Trim.java
@@ -0,0 +1,137 @@
+package kr.pe.kwonnam.freemarkerdynamicqlbuilder.directives.trim;
+
+import java.util.List;
+
+/**
+ * Trim template result string.
+ * <p/>
+ * This is thread safe but the setter methods must not be called while calling trim() method.
+ */
+public class Trim {
+    private String prefix;
+
+    private List<String> prefixOverrides;
+
+    private String suffix;
+
+    private List<String> suffixOverrides;
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    /**
+     * prefix will be added to the trimmed string's head if the trimmed string is not empty.
+     *
+     * @param prefix prefix for the result of trim
+     */
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
+    }
+
+    public List<String> getPrefixOverrides() {
+        return prefixOverrides;
+    }
+
+    /**
+     * After trimming, if the trimmed string starts with one of the prefixRemoves,
+     * only the first matching item from the head of the trimmed string will be removed.
+     *
+     * @param prefixOverrides list of strings that will be removed from the head of the trimmed string.
+     */
+    public void setPrefixOverrides(List<String> prefixOverrides) {
+        this.prefixOverrides = prefixOverrides;
+    }
+
+    public String getSuffix() {
+        return suffix;
+    }
+
+    /**
+     * suffix will be add to the trimmed string's tail if the trimmed string is not empty.
+     *
+     * @param suffix suffix for the result of trim
+     */
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public List<String> getSuffixOverrides() {
+        return suffixOverrides;
+    }
+
+    /**
+     * After trimming, if the trimmed string ends with one of the suffixRemoves,
+     * only the first matching item from the tail of the trimmed string will be removed.
+     *
+     * @param suffixOverrides list of strings that will be removed from the tail of the trimmed string.
+     */
+    public void setSuffixOverrides(List<String> suffixOverrides) {
+        this.suffixOverrides = suffixOverrides;
+    }
+
+    /**
+     * Trim the parameter string and apply configurations.
+     *
+     * @param str string to be trimmed
+     * @return trimmed string. null if str is null.
+     */
+    public String trim(String str) {
+        if (str == null) {
+            return null;
+        }
+
+        String trimmed = str.trim();
+
+        if (trimmed.isEmpty()) {
+            return trimmed;
+        }
+
+        trimmed = processPrefixOverride(trimmed);
+        trimmed = processSuffixOverride(trimmed);
+        trimmed = processPrefix(trimmed);
+        trimmed = processSuffix(trimmed);
+        return trimmed;
+    }
+
+    private String processPrefixOverride(String trimmed) {
+        if (prefixOverrides == null || prefixOverrides.isEmpty()) {
+            return trimmed;
+        }
+
+        for (String prefixOverride : prefixOverrides) {
+            if (trimmed.startsWith(prefixOverride)) {
+                return trimmed.substring(prefixOverride.length());
+            }
+        }
+        return trimmed;
+    }
+
+    private String processSuffixOverride(String trimmed) {
+        if (suffixOverrides == null || suffixOverrides.isEmpty()) {
+            return trimmed;
+        }
+
+        for (String suffixOverride : suffixOverrides) {
+            if (trimmed.endsWith(suffixOverride)) {
+                int idx = trimmed.lastIndexOf(suffixOverride);
+                return trimmed.substring(0, idx);
+            }
+        }
+        return trimmed;
+    }
+
+    private String processPrefix(String trimmed) {
+        if (prefix == null) {
+            return trimmed;
+        }
+        return prefix + trimmed;
+    }
+
+    private String processSuffix(String trimmed) {
+        if (suffix == null) {
+            return trimmed;
+        }
+        return trimmed + suffix;
+    }
+}
---
+++ src/test/java/kr/pe/kwonnam/freemarkerdynamicqlbuilder/directives/trim/TrimTest.java
@@ -0,0 +1,138 @@
+package kr.pe.kwonnam.freemarkerdynamicqlbuilder.directives.trim;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class TrimTest {
+    private Logger log = LoggerFactory.getLogger(TrimTest.class);
+
+    private Trim trim;
+
+    @Before
+    public void setUp() throws Exception {
+        trim = new Trim();
+    }
+
+    public List<String> list(String... str) {
+        return Arrays.asList(str);
+    }
+
+    @Test
+    public void trim_null() throws Exception {
+        assertThat(trim.trim(null), nullValue());
+    }
+
+    @Test
+    public void trim_to_empty() throws Exception {
+        assertThat(trim.trim(""), is(""));
+        assertThat(trim.trim("  \t \n \r  "), is(""));
+    }
+
+    @Test
+    public void trim_no_options() throws Exception {
+        assertThat(trim.trim("  \r \t \n hello \r \t \n world \t \r \n  "), is("hello \r \t \n world"));
+        assertThat(trim.trim("  \r \n AND username = ? AND email = ?  \r \n  \t  "), is("AND username = ? AND email = ?"));
+    }
+
+    @Test
+    public void trim_prefix_trimmed_empfy() throws Exception {
+        trim.setPrefix("WHERE ");
+        assertThat(trim.trim(" \r \t \n"), is(""));
+    }
+
+    @Test
+    public void trim_prefix_trimmed_not_empty() throws Exception {
+        trim.setPrefix("WHERE ");
+        assertThat(trim.trim(" \r \t username = ? \n"), is("WHERE username = ?"));
+
+        trim.setPrefix("[");
+        assertThat(trim.trim(" \r \t username = ? \n"), is("[username = ?"));
+    }
+
+    @Test
+    public void trim_prefixOverrides_nothing_to_override() throws Exception {
+        trim.setPrefixOverrides(list("and ", "or "));
+        assertThat(trim.trim(" something = ?  \n"), is("something = ?"));
+    }
+
+    @Test
+    public void trim_prefixOverrides_first_item() throws Exception {
+        trim.setPrefixOverrides(list("and ", "or "));
+        assertThat(trim.trim("  and or etc... "), is("or etc..."));
+    }
+
+    @Test
+    public void trim_prefixOverrides_last_item() throws Exception {
+        trim.setPrefixOverrides(list("AND ", "ELSE ", "OR "));
+        assertThat(trim.trim("\t OR something=? AND somewhere=? \n"), is("something=? AND somewhere=?"));
+    }
+
+    @Test
+    public void trim_prfix_and_prefixOverrides_same() throws Exception {
+        trim.setPrefix("WHERE ");
+        trim.setPrefixOverrides(list("WHERE "));
+
+        assertThat(trim.trim("\t something=? AND somewhere=? \n"), is("WHERE something=? AND somewhere=?"));
+    }
+
+    @Test
+    public void trim_suffix_trimmed_empty() throws Exception {
+        trim.setSuffix(" END");
+        assertThat(trim.trim("\t \r \n "), is(""));
+    }
+
+    @Test
+    public void trim_suffix_trimmed_not_empty() throws Exception {
+        trim.setSuffix(" END");
+        assertThat(trim.trim("  \t a=?, b=? \n "), is("a=?, b=? END"));
+
+        trim.setSuffix("]");
+        assertThat(trim.trim("  \t a=?, b=? \n "), is("a=?, b=?]"));
+    }
+
+    @Test
+    public void trim_suffixOverrides_nothing_to_override() throws Exception {
+        trim.setSuffixOverrides(list(",", "|"));
+        assertThat(trim.trim(" \t a=?, b=c  "), is("a=?, b=c"));
+    }
+
+    @Test
+    public void trim_suffixOverides_first_item() throws Exception {
+        trim.setSuffixOverrides(list(",", "|"));
+        assertThat(trim.trim("  a=?, b=c, \t\r\n"), is("a=?, b=c"));
+    }
+
+    @Test
+    public void trim_suffixOverrides_last_item() throws Exception {
+        trim.setSuffixOverrides(list(",", "===", "|"));
+        assertThat(trim.trim(" \t a=? | b=c | d=?,|"), is("a=? | b=c | d=?,"));
+    }
+
+    @Test
+    public void trim_suffix_and_suffixOverrides_same() throws Exception {
+        trim.setSuffix("]");
+        trim.setSuffixOverrides(list("]"));
+
+        assertThat(trim.trim("[hello world"), is("[hello world]"));
+    }
+
+    @Test
+    public void trim_all_options() throws Exception {
+        trim.setPrefix("[ ");
+        trim.setPrefixOverrides(list("AND ", "OR "));
+        trim.setSuffix(" ]");
+        trim.setSuffixOverrides(list(",", "|"));
+
+        assertThat(trim.trim("  \t AND hello=world,  \r\n "), is("[ hello=world ]"));
+        assertThat(trim.trim("  \t OR hello=world|  \r\n "), is("[ hello=world ]"));
+    }
+}
